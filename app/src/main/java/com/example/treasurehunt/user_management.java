d8c0002bbf0a41ad204782e4832d712e9e142c39
Bidhan Arya <bidhanar@iitk.ac.in>
2022-12-21 21:26:23 +0530

    add user and location adding functionality

--- app/src/main/java/com/example/treasurehunt/user_management.java
+++ app/src/main/java/com/example/treasurehunt/user_management.java
@@ -2,19 +2,45 @@ package com.example.treasurehunt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.parse.SignUpCallback;
 
 public class user_management extends AppCompatActivity {
+
+
+    Button add_user_location;
+    TextView orbutton;
+    EditText add_username;
+    EditText add_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_management);
+
+
+        add_user_location = findViewById(R.id.add_user_location);
+        orbutton = findViewById(R.id.or_button);
+        add_username = findViewById(R.id.add_username);
+        add_password = findViewById(R.id.add_password);
 
         ColorDrawable cd = new ColorDrawable(Color.parseColor("#043348"));
 //        ColorDrawable cd = new ColorDrawable(R.drawable.header_background);
@@ -30,5 +56,98 @@ public class user_management extends AppCompatActivity {
             window.setStatusBarColor(this.getResources().getColor(R.color.theme));
         }
 
+
+        //add functionality to text or_button
+        orbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                change(view);
+            }
+        });
+
+        //add user and location adding facility
+        add_user_location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(orbutton.getText().toString().equals("Add Location")){
+                    add_user(view);
+
+                }
+                else{
+                    add_location(view);
+                }
+            }
+        });
+
     }
+
+    public void change(View view){
+        if(orbutton.getText().toString().equals("Add Location")){
+            orbutton.setText("Add User");
+            add_password.setVisibility(View.INVISIBLE);
+            add_username.setHint("Location");
+            add_user_location.setText("Add Location");
+        }
+        else{
+            orbutton.setText("Add Location");
+            add_password.setVisibility(View.VISIBLE);
+            add_username.setHint("Username");
+            add_user_location.setText("Add User");
+        }
+    }
+
+    public void add_user(View view){
+        if(add_username.getText().toString().matches("") || add_password.getText().toString().matches("")){
+            Toast.makeText(getApplicationContext(),"Username and Password are required!!", Toast.LENGTH_SHORT).show();
+        }else{
+            ParseUser user = new ParseUser();
+            user.setUsername(add_username.getText().toString());
+            user.setPassword(add_password.getText().toString());
+
+            user.signUpInBackground(new SignUpCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e == null){
+                        Toast.makeText(getApplicationContext(),"User added successfully!",Toast.LENGTH_SHORT).show();
+                        add_username.setText("");
+                        add_username.setHint("Username");
+                        add_password.setText("");
+                        add_password.setHint("Password");
+                    }
+                    else{
+                        Log.i("Sign Up","failed");
+                        Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    public void add_location(View view){
+        if(add_username.getText().toString().matches("")){
+            Toast.makeText(getApplicationContext(),"Add valid location",Toast.LENGTH_SHORT).show();
+        }
+        else{
+//            ParseObject parseObject = new ParseObject("teams");
+            ParseObject parseObject = new ParseObject("locations");
+            parseObject.put("locations",add_username.getText().toString().replaceAll("\\s", ""));
+            parseObject.saveInBackground(new SaveCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if(e == null){
+                        Toast.makeText(getApplicationContext(),"Location added successfully",Toast.LENGTH_SHORT).show();
+                        add_username.setText("");
+                        add_username.setHint("Location");
+                    }
+                    else{
+                        Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+//            location.put("locations" , add_username.getText().toString().replaceAll("\\s", ""));
+//            location.saveInBackground();
+        }
+    }
+
+
 }
